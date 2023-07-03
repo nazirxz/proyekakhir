d4cf936acf2b2cbf66494198435390b150927d90
Nazirman <nzryanzr@gmail.com>
2023-07-03 11:28:31 +0700

    LensFragment didnt work / update dummyHewan

--- app/src/main/java/com/nazirman/proyekakhir/ui/hewan/dummyHewan.kt
+++ app/src/main/java/com/nazirman/proyekakhir/ui/hewan/dummyHewan.kt
@@ -5,28 +5,154 @@ import com.nazirman.proyekakhir.data.hewan.Hewan
 
 
 object dummyHewan {
-    private val namaHewan = arrayOf("Ayam","Burung")
-    private val deskripsiAyam = "Ayam adalah hewan unggas yang memiliki bulu berwarna-warni. Mereka suka berjalan dengan langkah-ngetap yang lucu dan bisa bertelur untuk membuat anak ayam baru."
-    private val habitatAyam = "Ayam biasanya tinggal di kandang atau peternakan."
+    private val namaHewan = arrayOf("Ayam","Burung","Cicak","Domba","Elang","Flamingo","Gajah","Harimau","Ikan","Jerapah","Kambing","Kucing","Kelinci",
+        "Kupu-Kupu","Sapi","Monyet","Panda","Rusa","Ular","Zebra")
+    private val deskripsiAyam = "Ayam adalah hewan unggas yang memiliki bulu berwarna-warni. Mereka suka berjalan dengan langkah-ngetap yang lucu dan bisa bertelur untuk membuat anak ayam baru"
+    private val habitatAyam = "Ayam biasanya tinggal di kandang atau peternakan"
 
+    private val habitatBurung = "Burung dapat ditemukan di berbagai tempat seperti hutan, taman, dan di dekat rumah kita"
+    private val deskripsiBurung = "Burung adalah hewan yang bisa terbang. Mereka memiliki sayap yang kuat dan bisa membuat sarang untuk tempat tinggal mereka. Setiap burung memiliki warna dan suara yang berbeda-beda, dan mereka sering bernyanyi di pagi hari"
 
-    private val habitatBurung = "Burung dapat ditemukan di berbagai tempat seperti hutan, taman, dan di dekat rumah kita."
-    private val deskripsiBurung = "Burung adalah hewan yang bisa terbang. Mereka memiliki sayap yang kuat dan bisa membuat sarang untuk tempat tinggal mereka. Setiap burung memiliki warna dan suara yang berbeda-beda, dan mereka sering bernyanyi di pagi hari."
+    private val deskripsiCicak = "Cicak sendiri adalah hewan reptil yang biasa merayap di dinding atau pohon. Cicak berwarna abu-abu, tetapi ada pula yang berwarna coklat kehitam-hitaman. Cicak biasanya berukuran sekitar 10 centimeter"
+    private val habitatCicak = "Cicak sering terlihat di sekitar rumah atau di pepohonan"
+
+    private val deskripsiDomba = "Domba adalah hewan ternak yang memiliki bulu lembut dan tebal. Mereka suka menggembalakan dan makan rumput. Domba juga memberikan kita wol yang digunakan untuk membuat pakaian hangat seperti sweater dan syal"
+    private val habitatDomba = "Domba tinggal di padang rumput atau peternakan"
+
+    private val deskripsiElang = "Elang adalah burung yang memiliki sayap lebar dan cakar yang kuat. Mereka bisa terbang sangat tinggi di langit dan memiliki penglihatan yang tajam. Elang adalah simbol kekuatan dan kebebasan"
+    private val habitatElang = "Elang sering ditemukan di pegunungan dan daerah terbuka yang tinggi"
+
+    private val deskripsiFlamingo = "Flamingo adalah burung yang memiliki bulu berwarna merah muda atau oranye. Mereka memiliki leher yang panjang dan kaki yang tinggi. Flamingo suka mencari makanan di air, seperti ikan-ikan kecil, udang, dan kerang"
+    private val habitatFlamingo = "Flamingo hidup di rawa-rawa dan danau dengan air yang dangkal"
+
+    private val deskripsiGajah = "Gajah adalah hewan mamalia terbesar di darat. Mereka memiliki tubuh yang besar, telinga yang lebar, dan belalai yang panjang. Gajah sangat cerdas dan suka hidup dalam kelompok besar yang disebut kawanan"
+    private val habitatGajah = "Gajah tinggal di hutan dan padang rumput di Afrika dan Asia"
+
+    private val deskripsiHarimau = "Harimau adalah kucing terbesar di dunia. Mereka memiliki belang-beling di tubuh mereka yang membantu mereka bersembunyi di antara rumput tinggi. Harimau adalah pemburu yang tangguh dan bisa melompat jauh untuk menangkap mangsa"
+    private val habitatHarimau = "Harimau hidup di hutan-hutan tropis dan hutan bambu di Asia."
+
+    private val deskripsiIkan = "Ikan adalah hewan yang hidup di dalam air. Mereka memiliki sirip dan insang untuk bernapas di air. Ada banyak jenis ikan dengan warna, bentuk, dan ukuran yang berbeda. Beberapa ikan bisa berenang sangat cepat, sementara yang lain bisa bersembunyi di antara terumbu karang"
+    private val habitatIkan = "Ikan hidup di air, seperti di sungai, danau, dan samudra"
+
+    private val deskripsiJerapah = "Jerapah adalah hewan dengan leher yang sangat panjang dan memiliki kaki yang tinggi. Mereka bisa mencapai daun-daun pohon yang tinggi untuk makan. Jerapah memiliki lidah yang panjang dan bisa menggapai daun-daun di atas pohon"
+    private val habitatJerapah = "Jerapah hidup di padang rumput dan hutan di Afrika"
+
+    private val deskripsiKambing = "Kambing adalah hewan ternak yang memiliki tanduk kecil di kepalanya. Mereka suka makan rumput dan daun-daunan. Kambing juga memberikan kita susu yang bisa diolah menjadi keju dan yoghurt"
+    private val habitatKambing = "Kambing tinggal di pegunungan, padang rumput, dan peternakan"
+
+    private val deskripsiKucing = "Kucing adalah hewan peliharaan yang lucu dan lincah. Mereka memiliki bulu yang lembut dan mata yang tajam. Kucing suka bermain dengan bola atau tali, dan mereka bisa menjilati diri sendiri dengan lidah yang kasar"
+    private val habitatKucing = "Kucing bisa hidup di dalam rumah atau di luar rumah"
+
+    private val deskripsiKelinci = "Kelinci adalah hewan mamalia kecil yang memiliki telinga panjang dan kaki yang kuat. Mereka suka makan wortel dan rumput. Kelinci juga bisa melompat tinggi dan cepat"
+    private val habitatKelinci = "Kelinci biasanya tinggal di padang rumput atau di bawah tanah"
+
+    private val deskripsiKupu2 = "Kupu-kupu adalah serangga yang memiliki sayap berwarna-warni dan indah. Mereka mengubah diri dari ulat menjadi kepompong dan akhirnya menjadi kupu-kupu. Kupu-kupu suka menghisap nectar bunga dengan belalainya yang panjang"
+    private val habitatKupu2 = "Kupu-kupu bisa ditemukan di berbagai tempat seperti taman dan kebun bunga"
+
+    private val deskripsiSapi = "Sapi adalah hewan ternak yang memiliki tubuh besar dan tanduk di kepalanya. Mereka memberikan kita susu yang bisa diminum dan daging yang bisa dimakan. Sapi juga digunakan untuk membajak ladang atau mengangkut beban"
+    private val habitatSapi = "Sapi biasanya tinggal di peternakan atau padang rumput"
+
+    private val deskripsiMonyet = "Monyet adalah hewan primata yang lincah dan cerdas. Mereka memiliki ekor yang panjang dan bisa bergelantungan di pepohonan. Monyet suka makan buah-buahan dan kadang-kadang suka memainkan trik-trik lucu"
+    private val habitatMonyet = "Monyet bisa ditemukan di hutan-hutan tropis di berbagai negara"
+
+    private val deskripsiPanda = "Panda adalah hewan yang lucu dengan bulu hitam dan putih. Mereka suka makan bambu dan memiliki kebiasaan tidur yang lama. Panda adalah simbol keselamatan dan dilindungi karena mereka langka"
+    private val habitatPanda = "Panda hidup di hutan bambu di Tiongkok"
+
+    private val deskripsiRusa = "Rusa adalah hewan mamalia yang memiliki tanduk di kepala. Mereka suka makan rumput dan daun-daunan. Rusa jantan memiliki tanduk yang besar dan digunakan untuk bertarung dalam musim kawin"
+    private val habitatRusa = "Rusa biasanya hidup di hutan-hutan dan padang rumput"
+
+    private val deskripsiUlar = " Ular adalah reptil yang panjang dan licin. Mereka tidak memiliki kaki, tetapi bisa meluncur dengan lincah. Ada banyak jenis ular dengan warna, ukuran, dan pola yang berbeda. Beberapa ular bisa berbisa, sementara yang lain tidak berbahaya bagi manusia."
+    private val habitatUlar = "Ular bisa ditemukan di berbagai habitat seperti hutan, padang rumput, dan lahan basah"
+
+    private val deskripsiZebra = "Zebra adalah hewan yang memiliki tubuh berwarna hitam dan putih dengan garis-garis yang khas. Mereka memiliki kaki yang kuat dan bisa berlari dengan cepat. Zebra hidup dalam kawanan besar dan menggunakan garis-garis mereka untuk melindungi diri dari predator. Mereka suka makan rumput dan daun-daunan di padang rumput"
+    private val habitatZebra = "Zebra biasanya ditemukan di padang rumput Afrika"
 
     private val deskripsiHewan = arrayOf(
         deskripsiAyam,
-        deskripsiBurung)
+        deskripsiBurung,
+        deskripsiCicak,
+        deskripsiDomba,
+        deskripsiElang,
+        deskripsiFlamingo,
+        deskripsiGajah,
+        deskripsiHarimau,
+        deskripsiIkan,
+        deskripsiJerapah,
+        deskripsiKambing,
+        deskripsiKucing,
+        deskripsiKelinci,
+        deskripsiKupu2,
+        deskripsiSapi,
+        deskripsiMonyet,
+        deskripsiPanda,
+        deskripsiRusa,
+        deskripsiUlar,
+        deskripsiZebra)
 
     private val habitatHewan = arrayOf(
         habitatAyam,
-        habitatBurung
-    )
+        habitatBurung,
+        habitatCicak,
+        habitatDomba,
+        habitatElang,
+        habitatFlamingo,
+        habitatGajah,
+        habitatHarimau,
+        habitatIkan,
+        habitatJerapah,
+        habitatKambing,
+        habitatKucing,
+        habitatKelinci,
+        habitatKupu2,
+        habitatSapi,
+        habitatMonyet,
+        habitatPanda,
+        habitatRusa,
+        habitatUlar,
+        habitatZebra)
 
     private val gambarHewan = arrayOf<Int>(R.drawable.hayam,
-        R.drawable.hburung)
+        R.drawable.hburung,
+        R.drawable.hcicak,
+        R.drawable.hdomba,
+        R.drawable.helang,
+        R.drawable.hflamingo,
+        R.drawable.hgajah,
+        R.drawable.hharimau,
+        R.drawable.hikan,
+        R.drawable.hjerapah,
+        R.drawable.hkambing,
+        R.drawable.hkucing,
+        R.drawable.hkelinci,
+        R.drawable.hkupu2,
+        R.drawable.hsapi,
+        R.drawable.hmonyet,
+        R.drawable.hpanda,
+        R.drawable.hrusa,
+        R.drawable.hular,
+        R.drawable.hzebra
+    )
 
     private val gambarHabitat = arrayOf<Int>(R.drawable.hayam,
-        R.drawable.hburung)
+        R.drawable.hburung,
+        R.drawable.habcicak,
+        R.drawable.habdomba,
+        R.drawable.habelang,
+        R.drawable.habflamingo,
+        R.drawable.habgajah,
+        R.drawable.habharimau,
+        R.drawable.habikan,
+        R.drawable.habjerapah,
+        R.drawable.habkambing,
+        R.drawable.habkambing,
+        R.drawable.habkucing,
+        R.drawable.habkupu2,
+        R.drawable.habsapi,
+        R.drawable.habmonyet,
+        R.drawable.habpanda,
+        R.drawable.habrusa,
+        R.drawable.habular,
+        R.drawable.habzebras)
 
     val listData: ArrayList<Hewan>
         get() {
--- app/src/main/java/com/nazirman/proyekakhir/ui/lens/LensFragment.kt
+++ app/src/main/java/com/nazirman/proyekakhir/ui/lens/LensFragment.kt
@@ -1,27 +1,37 @@
 package com.nazirman.proyekakhir.ui.lens
 
+import TFLiteHelper
+import android.Manifest
 import android.content.pm.PackageManager
+import android.content.res.AssetManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.Manifest
 import com.nazirman.proyekakhir.R
 import com.nazirman.proyekakhir.databinding.FragmentLensBinding
+
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+
 class LensFragment : Fragment() {
     private lateinit var binding: FragmentLensBinding
-    private lateinit var cameraExecutor: ExecutorService
-    private val CAMERA_PERMISSION_REQUEST_CODE = 100
-
+//    private lateinit var cameraExecutor: ExecutorService
+//    private val CAMERA_PERMISSION_REQUEST_CODE = 100
+//    private lateinit var interpreter: Interpreter
+//    private lateinit var tfliteHelper: TFLiteHelper
+//
+//    private val categories = arrayOf("kupu-kupu", "kucing", "ayam", "burung", "gajah", "kelinci", "ikan", "kambing", "sapi", "jerapah") // Update with your categories
+//    private val numClasses = categories.size
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,132 +45,217 @@ class LensFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
-        requestCameraPermission() // Request camera permission before setting up the camera
-        buttonHandler()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        cameraExecutor.shutdown()
+//        requestCameraPermission() // Request camera permission before setting up the camera
+//
+////        // Load the TensorFlow Lite model
+////        interpreter = Interpreter(loadModelFile())
+//
+//        buttonHandler()
+//        // Initialize TFLiteHelper
+//        tfliteHelper = TFLiteHelper(requireContext())
+//        tfliteHelper.initialize()
     }
 
     private fun setupToolbar() {
-        val actionBar: androidx.appcompat.app.ActionBar? =
-            (activity as AppCompatActivity).supportActionBar
-        actionBar?.apply {
-            setTitle(R.string.title_lens)
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_arrow_back_24)
+//        val actionBar: androidx.appcompat.app.ActionBar? =
+//            (activity as AppCompatActivity).supportActionBar
+//        actionBar?.apply {
+//            setTitle(R.string.title_lens)
+//            setDisplayHomeAsUpEnabled(true)
+//            setHomeAsUpIndicator(R.drawable.ic_arrow_back_24)
+//        }
+//        setHasOptionsMenu(true)
+    }
+
+//    private fun requestCameraPermission() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (requireActivity().checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(
+//                    arrayOf(Manifest.permission.CAMERA),
+//                    CAMERA_PERMISSION_REQUEST_CODE
+//                )
+//            } else {
+//                // Permission already granted
+//                setupCamera()
+//            }
+//        }
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                // Camera permission granted
+//                setupCamera()
+//            } else {
+//                // Camera permission denied
+//                // Handle the case where the user denied the permission
+//            }
+//        } else {
+//            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        }
+//    }
+//
+//    private fun setupCamera() {
+//        // Initialize cameraExecutor
+//        cameraExecutor = Executors.newSingleThreadExecutor()
+//
+//        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+//
+//        cameraProviderFuture.addListener({
+//            // Used to bind the lifecycle of cameras to the lifecycle owner
+//            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+//
+//            // Preview
+//            val previewView = binding.cameraView
+//
+//            val preview = Preview.Builder().build().also {
+//                it.setSurfaceProvider(previewView.surfaceProvider)
+//            }
+//
+//            // Select back camera as a default
+//            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+//
+//            try {
+//                // Unbind use cases before rebinding
+//                cameraProvider.unbindAll()
+//
+//                // Bind use cases to camera
+//                val camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
+//
+//                // Log available camera devices
+//                val cameraInfo = camera.cameraInfo
+//                Log.d(TAG, "Camera Info: $cameraInfo")
+//
+//            } catch (exc: Exception) {
+//                Log.e(TAG, "Use case binding failed", exc)
+//            }
+//        }, ContextCompat.getMainExecutor(requireContext()))
+//    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+//        cameraExecutor.shutdown()
+//        tfliteHelper.close()
+    }
+//
+//    private fun buttonHandler() {
+//        binding.btnBack.setOnClickListener {
+//            requireActivity().finish()
+//        }
+//
+//        binding.btnUpload.setOnClickListener {
+//            // Get the current frame from the camera view
+//            val bitmap = binding.cameraView.bitmap
+//            if (bitmap != null) {
+//                tfliteHelper.classifyAsync(bitmap)
+//                    .addOnSuccessListener { result ->
+//                        // Display the prediction result
+//                        Toast.makeText(requireContext(), result, Toast.LENGTH_SHORT).show()
+//                    }
+//                    .addOnFailureListener { exception ->
+//                        // Handle classification failure
+//                        Toast.makeText(requireContext(), "Classification failed: ${exception.message}", Toast.LENGTH_SHORT).show()
+//                    }
+//            }
+//            if (bitmap != null) {
+//                // Preprocess the image
+//                val resizedImage = Bitmap.createScaledBitmap(bitmap, 80, 80, true)
+//                val normalizedImage = normalizeImage(resizedImage)
+//
+//                // Reshape the image to match the model's input shape
+//                val inputImage = normalizedImage.reshape(1, 80, 80, 3)
+//
+//                // Run inference on the image using your model
+//                val prediction = runInference(inputImage)
+//
+//                // Get the predicted class index
+//                val predictedClassIndex = prediction.indexOfFirst { it == prediction.maxOrNull() }
+//
+//                // Get the predicted class label
+//                val predictedClass = categories[predictedClassIndex]
+//
+//                // Display the predicted class to the user
+//                binding.tvResultRas.text = predictedClass
+//            } else {
+//                // Handle the case when the bitmap is null
+//                Toast.makeText(requireContext(), "Failed to capture image", Toast.LENGTH_SHORT).show()
+//            }
         }
-        setHasOptionsMenu(true)
-    }
+    
 
-    private fun requestCameraPermission() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                arrayOf(Manifest.permission.CAMERA),
-                CAMERA_PERMISSION_REQUEST_CODE
-            )
-        } else {
-            // Permission already granted
-            setupCamera()
-        }
-    }
+//    private fun normalizeImage(bitmap: Bitmap): FloatArray {
+//        val pixels = IntArray(bitmap.width * bitmap.height)
+//        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+//
+//        val normalizedImage = FloatArray(bitmap.width * bitmap.height * 3)
+//        for (i in pixels.indices) {
+//            val pixel = pixels[i]
+//            normalizedImage[i * 3] = ((pixel shr 16) and 0xFF) / 255.0f // Red channel
+//            normalizedImage[i * 3 + 1] = ((pixel shr 8) and 0xFF) / 255.0f // Green channel
+//            normalizedImage[i * 3 + 2] = (pixel and 0xFF) / 255.0f // Blue channel
+//        }
+//
+//        return normalizedImage
+//    }
+//
+//    private fun runInference(inputImage: Array<FloatArray>): FloatArray {
+//        val output = Array(1) { FloatArray(numClasses) }
+//        interpreter.run(inputImage, output)
+//        return output[0]
+//    }
+//
+//    private fun loadModelFile(): ByteBuffer {
+//        val assetManager: AssetManager = requireContext().assets
+//        val fileDescriptor = assetManager.openFd("model.tflite")
+//        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+//        val fileChannel = inputStream.channel
+//        val startOffset = fileDescriptor.startOffset
+//        val declaredLength = fileDescriptor.declaredLength
+//        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+//    }
+//
+//    private fun Array<FloatArray>.flatten(): FloatArray {
+//        val size = sumOf { it.size }
+//        val result = FloatArray(size)
+//        var offset = 0
+//
+//        for (array in this) {
+//            array.copyInto(result, offset)
+//            offset += array.size
+//        }
+//
+//        return result
+//    }
+//
+//    private fun FloatArray.reshape(vararg shape: Int): Array<FloatArray> {
+//        require(size == shape.reduce(Int::times)) { "Array size does not match the shape" }
+//
+//        val result = Array(shape[0]) { FloatArray(shape[1]) }
+//        var offset = 0
+//
+//        for (i in result.indices) {
+//            for (j in result[i].indices) {
+//                result[i][j] = this[offset++]
+//            }
+//        }
+//
+//        return result
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if (item.itemId == android.R.id.home) {
+//            requireActivity().finish()
+//            return true
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+//
+//    companion object {
+//        private const val TAG = "LensFragment"
+//    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Camera permission granted
-                setupCamera()
-            } else {
-                // Camera permission denied
-                // Handle the case where the user denied the permission
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-    }
 
-    private fun setupCamera() {
-        // Initialize cameraExecutor
-        cameraExecutor = Executors.newSingleThreadExecutor()
-
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val previewView = binding.cameraView
-
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(previewView.surfaceProvider)
-            }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                val camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
-
-                // Log available camera devices
-                val cameraInfo = camera.cameraInfo
-                Log.d(TAG, "Camera Info: $cameraInfo")
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-        }, ContextCompat.getMainExecutor(requireContext()))
-    }
-    private fun buttonHandler() {
-        binding.btnBack.setOnClickListener {
-            requireActivity().finish()
-        }
-
-        binding.btnUpload.setOnClickListener {
-            // TODO: Handle upload button click
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            requireActivity().finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            requireActivity().window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            requireActivity().window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
-        requireActivity().actionBar?.hide()
-    }
-
-    companion object {
-        private const val TAG = "LensFragment"
-    }
-}
